8978d0318c54931795620e52c44fe2124e4c5077
solvedbiscuit71 <ppraveen98841@gmail.com>
2022-05-07 10:40:43 +0530

    Changed Rust Structure.
    Move folder from Rust/src/ -> Rust/
    and Removed the Cargo.toml & target/

---
+++ Rust/main.rs
@@ -0,0 +1,13 @@
+/*
+	Routes:
+		basics/
+		ownership/
+		structs/
+		enums/
+
+	NOTE: here, folder will be lowercase & file will be snake_case.
+*/
+
+fn main() {
+    println!("Welcome to Rust Tutorial");
+}
