fa457889bdf3e38afb8882e67ba68b901e659f66
solvedbiscuit71 <ppraveen98841@gmail.com>
2022-05-13 09:40:26 +0530

    update Rust/main.rs

--- Rust/main.rs
+++ Rust/main.rs
@@ -1,27 +1,8 @@
 /*
-	Rust Tutorial
-		by Praveen Perumal
-
-	Routes:
-		Basic/
-		Ownership/
-		Enum/
-		Struct/
-		Generics/
-		Traits/
-		Smart_Pointer/
+    Rust Tutorial
+        by Praveen Perumal
 */
-
 fn main() {
-	println!("Rust Tutorial");
-	println!("  by Praveen Perumal\n");
-
-	println!("Routes:");
-	println!("  Basic/");
-	println!("  Ownership/");
-	println!("  Enum/");
-	println!("  Struct/");
-	println!("  Generics/");
-	println!("  Traits/");
-	println!("  Smart_Pointer/");
+    println!("Welcome to Rust Tutorial");
+    println!("\tby Praveen Perumal")
 }
