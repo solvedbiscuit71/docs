2c5f716d15763d4894a5e99b0c85bad3134d3683
solvedbiscuit71 <ppraveen98841@gmail.com>
2022-05-26 18:16:48 +0530

    Refactor Actix/Routing

--- Actix/Routing/src/config.rs
+++ Actix/Routing/src/config.rs
@@ -2,22 +2,6 @@ use crate::handler::{product, user};
 use actix_web::web;
 
 pub fn config_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::scope("/user")
-            .route("/{id}", web::get().to(user::get_user_by_id))
-            .service(
-                web::resource("")
-                    .route(web::get().to(user::get_users))
-                    .route(web::post().to(user::post_user)),
-            ),
-    )
-    .service(
-        web::scope("/product")
-            .route("/{id}", web::get().to(product::get_product_by_id))
-            .service(
-                web::resource("")
-                    .route(web::get().to(product::get_products))
-                    .route(web::post().to(product::post_product)),
-            ),
-    );
+    cfg.service(web::scope("/user").configure(user::config))
+        .service(web::scope("/product").configure(product::config));
 }
--- Actix/Routing/src/handler/product.rs
+++ Actix/Routing/src/handler/product.rs
@@ -4,12 +4,12 @@ use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 #[derive(Deserialize, Serialize)]
-pub struct Product {
+struct Product {
     name: String,
     quantity: u32,
 }
 
-pub async fn get_products() -> impl Responder {
+async fn get_products() -> impl Responder {
     web::Json(json!({
         "code": 200,
         "product": [
@@ -18,7 +18,7 @@ pub async fn get_products() -> impl Responder {
     }))
 }
 
-pub async fn post_product(new_product: web::Json<Product>) -> impl Responder {
+async fn post_product(new_product: web::Json<Product>) -> impl Responder {
     let id = random::<u16>();
 
     web::Json(json!({
@@ -28,7 +28,7 @@ pub async fn post_product(new_product: web::Json<Product>) -> impl Responder {
     }))
 }
 
-pub async fn get_product_by_id(id: web::Path<u16>) -> impl Responder {
+async fn get_product_by_id(id: web::Path<u16>) -> impl Responder {
     web::Json(json!({
         "code": 200,
         "id": id.clone(),
@@ -37,4 +37,13 @@ pub async fn get_product_by_id(id: web::Path<u16>) -> impl Responder {
             "quantity": "..."
         }
     }))
+}
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.route("/{id}", web::get().to(get_product_by_id))
+        .service(
+            web::resource("")
+                .route(web::get().to(get_products))
+                .route(web::post().to(post_product)),
+        );
 }
