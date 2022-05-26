fcb634217b37fac106f9ea53c6fd6c4ebbf98767
solvedbiscuit71 <ppraveen98841@gmail.com>
2022-05-26 10:55:39 +0530

    Update Actix/HelloWorld
    complete the main.rs explaining the basic setup of Server

--- Actix/HelloWorld/src/main.rs
+++ Actix/HelloWorld/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/")]
+async fn greet() -> &'static str {
+    "Hello World!"
+}
+
+async fn echo(input: web::Path<String>) -> impl Responder {
+    format!("Echo: {input}")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    /*
+     * HttpServer() -> is a factory i.e create this closure for individual threads!
+     * App() -> is create for every thread so, app state is valid in that thread only.
+     */
+
+    HttpServer::new(|| {
+        App::new()
+            .service(greet) // -> greet() at GET "/"
+            .service(web::resource("/echo/{input}").route(web::get().to(echo))) // -> echo() at GET "/echo/:input"
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
